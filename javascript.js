4f929518c2a439c62642d41e1deba5916d2a3bed
Your Name <ndimant@hotmail.com>
2023-03-13 09:56:07 +0200

    adding css and javascript functions

--- javascript.js
+++ javascript.js
@@ -1,35 +1,100 @@
+const display = document.querySelector('.display');
+const clearBtn = document.querySelector('.clear');
+const operatorBtns = document.querySelectorAll('.operator');
+const numberBtns = document.querySelectorAll('.number');
+const equalBtn = document.querySelector('.equal');
+
+// initialize variables
+let firstNum = null;
+let operator = null;
+let displayValue = '';
+
+// function to update display
+function updateDisplay(value) {
+  displayValue += value;
+  display.textContent = displayValue;
+}
+
+// function to clear display
+function clearDisplay() {
+  firstNum = null;
+  operator = null;
+  displayValue = '';
+  display.textContent = displayValue;
+}
+
+// function to perform operation
+function operate(operator, a, b) {
+  switch (operator) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      return a / b;
+    default:
+      return "Invalid operator!";
+  }
+}
+
+// event listener for number buttons
+numberBtns.forEach(button => {
+  button.addEventListener('click', () => {
+    updateDisplay(button.value);
+  });
+});
+
+// event listener for operator buttons
+operatorBtns.forEach(button => {
+  button.addEventListener('click', () => {
+    if (!firstNum) {
+      firstNum = parseFloat(displayValue);
+      operator = button.value;
+      displayValue = '';
+    } else {
+      const secondNum = parseFloat(displayValue);
+      const result = operate(operator, firstNum, secondNum);
+      firstNum = result;
+      operator = button.value;
+      displayValue = '';
+      display.textContent = result;
+    }
+  });
+});
+
+// event listener for equal button
+equalBtn.addEventListener('click', () => {
+  if (firstNum && operator) {
+    const secondNum = parseFloat(displayValue);
+    const result = operate(operator, firstNum, secondNum);
+    firstNum = null;
+    operator = null;
+    displayValue = '';
+    display.textContent = result;
+  }
+});
+
+// event listener for clear button
+clearBtn.addEventListener('click', clearDisplay);
+
+// Define the add, subtract, multiply, and divide functions
 function add(a, b) {
-	return a + b;
+  return a + b;
 }
 
 function subtract(a, b) {
-	return a - b;
+  return a - b;
 }
 
 function multiply(a, b) {
-	return a * b;
+  return a * b;
 }
 
 function divide(a, b) {
-	if (b === 0) {
-		return "Error: Cannot divide by zero";
-	} else {
-		return a / b;
-	}
-}
-
-function operate(operator, a, b) {
-    switch (operator) {
-      case '+':
-        return add(a, b);
-      case '-':
-        return subtract(a, b);
-      case '*':
-        return multiply(a, b);
-      case '/':
-        return divide(a,b);
-      default:
-        return "Invalid operator!";
-    }
+  if (b === 0) {
+    return "Cannot divide by zero!";
   }
-  
+  return a / b;
+}
